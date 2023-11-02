279843ffe09d81d2b2e3db73a2f3a70a2739f307
Jochem <info@jochemkeller.nl>
2023-11-02 21:22:19 +0100

    feat: add support for vscode-remotes

--- code.sh
+++ code.sh
@@ -1,19 +1,30 @@
 : ${root:=~/Developer}
+
 : ${mindepth:=2}
 : ${maxdepth:=2}
 
-find -L "${root}" -type d -mindepth "${mindepth}" -maxdepth "${maxdepth}" | sed -e 's|/Users/jochem/Developer/||' | fzf --exact --filter="$1" | jq -nR '
+: ${remotes:=''}
+: ${remotedirs:=''}
+
+pattern="code -n --remote ssh-remote+"
+
+remotesmap=""
+
+while IFS= read -r -u3 x; IFS= read -r -u4 y; do
+  remotesmap+="$pattern$x $y\n"
+done 3<<<"$remotes" 4<<<"$remotedirs"
+
+find -L "${root}" -type d -mindepth "${mindepth}" -maxdepth "${maxdepth}" | sed -e 's|/Users/jochem/Developer/||' | (cat;echo $remotesmap) | fzf --exact --filter="$1" | jq -nR --arg jq_pattern "$pattern" '
 	[inputs]
 	|
 	map({
-		uid: ("/Users/jochem/Developer/" + .),
-		title: split("/")[-1],
-		subtitle: "~/Developer/\(split("/")[0])",
-		arg: ("/Users/jochem/Developer/" + .),
-		autocomplete: ("/Users/jochem/Developer/" + .),
+		uid: (. |= if contains($jq_pattern) then . else "/Users/jochem/Developer/" + . end),
+		title: (. |= if contains($jq_pattern) then . else split("/")[-1] end) | gsub("\($jq_pattern)"; "") | gsub("\\+"; "") | split("/")[0],
+		subtitle: (. |= if contains($jq_pattern) then . else "~/Developer/\(split("/")[0])" end),
+		arg: (. |= if contains($jq_pattern) then . else "/Users/jochem/Developer/" + . end),
+		autocomplete: (. |= if contains($jq_pattern) then . else "/Users/jochem/Developer/" + . end),
 		icon: {
-			type: "fileicon",
-			path: ("/Users/jochem/Developer/" + .)
+			path: (. |= if contains($jq_pattern) then "./ssh.png" else "./vs-code.png" end)
 		}
 	})
 	|
